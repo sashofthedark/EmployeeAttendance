b02a0e29130d0a3b50e251780960021d640f7008
Alexandra Bakman <sasha.bakman@gmail.com>
2020-06-14 21:15:17 +0300

    Added add to EmployeeList class

--- Utils.py
+++ Utils.py
@@ -9,3 +9,18 @@ class EmployeeList():
     def CreateEmployeeList(self):
         self.list = []
              
+    def add(self,employee):
+        Added = False
+        a = [(ind,value) for (ind,value) in enumerate(self.list) if value.id == employee.id]
+        if a!=[]:
+            raise ValueError('Employee already in database!')
+        elif type(employee.id) != int or len(employee.id) != 4:
+            raise ValueError('Invalid id, should be 4 digits long')
+        elif type(employee.age) != int or (employee.age >=18 and employee.age <= 67):
+            raise ValueError('Invalid age, should be between 18 and 67')
+        elif type(employee.phone) != int or len(employee.phone) != 10:
+            raise ValueError('Invalid phone number, should be 10 digits long')
+        else:
+            self.list.append(employee)
+            Added = True
+        return Added

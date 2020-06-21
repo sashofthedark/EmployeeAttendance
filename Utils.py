27ef4a6b39f7113e232d640dcf2348f3cd861cfc
Alexandra Bakman <sasha.bakman@gmail.com>
2020-06-21 21:21:31 +0300

    used openpyxl for reading xlsx files in addFromFile

--- Utils.py
+++ Utils.py
@@ -7,43 +7,52 @@ class Employee():
 
 class EmployeeList():
     def CreateEmployeeList(self):
-        self.list = []
-             
-    def add(self,id,name,phone,age):
-        Added = False
-
         try:
             self.list
         except NameError:
-            self.CreateEmployeeList()
-
+            self.list = []
+    
+    def check(self,id,name,phone,age):
+        #does this function "see" the self.list?
+        #checks whether the data for the employee is valid and he's not already in the employee list
+        CreateEmployeeList()
         a = [(ind,value) for (ind,value) in enumerate(self.list) if value.id == id]
         if a!=[]:
-            raise ValueError('Employee already in database!')
+            raise ValueError('Employee is already in database!')
         elif type(id) != int or len(str(id)) != 4:
-            raise ValueError('Invalid id, should be 4 digits long')
-        elif type(age) != int or not (int(age) >=18 and int(age) <= 67):
-            raise ValueError('Invalid age, should be between 18 and 67')
-        elif type(phone) != int or len(str(phone)) != 10:
-            raise ValueError('Invalid phone number, should be 10 digits long')
+            raise ValueError('Invalid ID')
+        elif type(name)!=str:
+            raise ValueError('Invalid name')
+        elif type(phone)!=int or len(str(phone))!=10:
+            raise ValueError('Invalid phone number')
+        elif type(age)!=int or int(age)<18 or int(age)>67:
+            raise ValueError('Invalid age')
+        else:
+            return True
+
+         
+    def add(self,id,name,phone,age):
+        CreateEmployeeList()
+        try:
+            check(self,id,name,phone,age)
+        except NameError as NamErr:
+            print('Error message was {}'.format(NamErr))
+            return False
         else:
             self.list.append(Employee(id,name,phone,age))
-            Added = True
-        return Added
+            return True
 
     def addFromFile(self,filepath):
         import csv
         import os
+        import openpyxl
 
         Added  = False
-        try:
-            self.list
-        except NameError:
-            self.CreateEmployeeList()
-        filetype = filepath[-4:]
-        if filetype!='.csv' and filetype!='.xls':
+        CreateEmployeeList()
+
+        if filepath[-4:]!='.csv' and filepath[-5:]!='.xlsx':
             raise TypeError('Wrong file type')
-        elif filetype == 'csv':
+        elif filepath[-4:] == '.csv':
             with open(filepath) as csv_file:
                 csv_reader = csv.reader(csv_file,delimiter=',')
                 if os.stat(filepath).st_size == 0:
@@ -51,20 +60,26 @@ class EmployeeList():
                 else:
                     row_cnt = 1
                     for row in csv_reader:
-                        if row[0].type !=int or len(str(row[0]))!= 4:
-                            raise ValueError('Wrong ID in row' + str(row_cnt))
-                        elif row[1].type !=str:
-                            raise ValueError('Name is not a string in row' + str(row_cnt))
-                        elif row[2].type != int or len(str(row[2]))!= 10:
-                            raise ValueError('Phone invalid in row ' + str(row_cnt))
-                        elif row[3].type != int or int(row[3]) < 18 or int(row[3]) > 67:
-                            raise ValueError('invalid age in row ' + str(row_cnt))
+                        try:
+                            check(self,row[0],row[1],row[2],row[3])
+                        except NameError as NamErr:
+                            print('Error message is {}'.format(NamErr) + ' in row ' + str(row_cnt))
                         else:
-                            CurrentEmployee = Employee(row[0],row[1],row[2],row[3])
-                         a = [(ind,value) for (ind,value) in enumerate(self.list) if value.id == CurrentEmployee.id]
-                         #check whether the id is already in the database
-                         if a!=[]:
-                             raise ValueError('Employee already in database!')
+                            add(self,row[0],row[1],row[2],row[3])
+        elif filepath[-5:] == '.xlsx':
+            wb_obj = openpyxl.load_workbook(filepath)
+            sheet_obj = wb_obj.active 
+            for rows in range(0,sheet_obj.max_row-1):
+                idc = sheet_obj.cell(row = rows,column = 0)
+                name = str(sheet_obj.cell(row = rows,column = 1))
+                phone = sheet_obj.cell(row = rows,column = 2)
+                age = sheet_obj.cell(row = rows,column = 3)
+                try:
+                    check(self,idc,name,phone,age)
+
+                
+
+
 
 
             

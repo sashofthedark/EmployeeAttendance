619a25ad0ca83762dea56651127347f031c7ec9c
Alexandra Bakman <sasha.bakman@gmail.com>
2020-07-09 21:06:45 +0300

    repush

--- Reports.py
+++ Reports.py
@@ -4,3 +4,13 @@ import csv
 import os
 
 class Attendance():
+    def CreateAttList(self):
+        path = './Attlist.csv'
+        if os.path.isfile(path):
+            pass
+        else:
+            with open(path,'w') as File:
+                pass 
+
+
+
